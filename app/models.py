90bf85c9aa974dac56c1d268ae7a0c85dba91cd8
Waseem Jan <syedwaseemjan@gmail.com>
2025-05-16 16:03:53 +0500

    Add models

--- app/models.py
+++ app/models.py
@@ -1,1 +1,34 @@
+from sqlalchemy import Column, Integer, String, Float, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
 from app.database import Base
+import datetime
+
+
+class Product(Base):
+    __tablename__ = "products"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(100), unique=True, index=True, nullable=False)
+    category = Column(String(50), index=True)
+    price = Column(Float, nullable=False)
+    inventory_count = Column(Integer, default=0)
+
+    sales = relationship("Sale", back_populates="product")
+
+
+class Sale(Base):
+    __tablename__ = "sales"
+    id = Column(Integer, primary_key=True, index=True)
+    product_id = Column(Integer, ForeignKey("products.id"))
+    quantity = Column(Integer, nullable=False)
+    total_price = Column(Float, nullable=False)
+    date = Column(DateTime, default=datetime.datetime.utcnow)
+
+    product = relationship("Product", back_populates="sales")
+
+
+class InventoryChange(Base):
+    __tablename__ = "inventory_changes"
+    id = Column(Integer, primary_key=True, index=True)
+    product_id = Column(Integer, ForeignKey("products.id"))
+    change = Column(Integer)  # positive or negative
+    date = Column(DateTime, default=datetime.datetime.utcnow)
