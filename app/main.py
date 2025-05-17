d60454ce61c1f7de345ba5af283e2950dfd1bd7c
Waseem Jan <syedwaseemjan@gmail.com>
2025-05-17 17:16:22 +0500

    Try to load env variables

--- app/main.py
+++ app/main.py
@@ -1,7 +1,11 @@
-from fastapi import FastAPI
+from dotenv import load_dotenv
 
-from app.config import API_PREFIX, DEBUG, PROJECT_NAME, VERSION
-from app.routers import api_router
+load_dotenv()
+
+from fastapi import FastAPI  # noqa: E402
+
+from app.config import API_PREFIX, DEBUG, PROJECT_NAME, VERSION  # noqa: E402
+from app.routers import api_router  # noqa: E402
 
 
 def get_application() -> FastAPI:
