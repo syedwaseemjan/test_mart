fb2dfa65495b25f65022a6effe903b8221350568
Waseem Jan <syedwaseemjan@gmail.com>
2025-05-17 17:13:34 +0500

    Raise error if DB url is not found

--- app/database.py
+++ app/database.py
@@ -1,16 +1,12 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DB_USER = "test_mart"
-DB_PASSWORD = "test_mart"
-DB_HOST = "localhost"
-DB_PORT = "3306"
-DB_NAME = "ecommerce_db"
-
-SQLALCHEMY_DATABASE_URL = (
-    f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-)
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+if not SQLALCHEMY_DATABASE_URL:
+    raise ValueError("DATABASE_URL must be set")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
